94d61966c1ebee708d9ec9528ec6a91328a75a6f
LotusBoard Admin <admin@lotusboard.local>
2025-11-30 19:57:06 +0330

    Исправлена проблема с регистрозависимостью роли администратора в SessionManager

--- ShelterAppProduction/Services/SessionManager.cs
+++ ShelterAppProduction/Services/SessionManager.cs
@@ -6,6 +6,6 @@ namespace ShelterAppProduction.Services
     {
         public static User CurrentUser { get; set; }
         public static bool IsAuthenticated => CurrentUser != null;
-        public static bool IsAdmin => CurrentUser != null && CurrentUser.Role == "Admin";
+        public static bool IsAdmin => CurrentUser != null && CurrentUser.Role == "admin";
     }
 }
